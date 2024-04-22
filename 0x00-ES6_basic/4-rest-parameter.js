7925a28815059993c7db7e640b2f57840cf03f3f
kazeem Tiamiyu <katiamiyu@gmail.com>
2024-04-22 19:50:32 +0000

    task 5 index 4

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,9 +1,3 @@
-export default function returnHowManyArguments(...theArgs) {
-  let total = 0;
-
-  for (const arg of theArgs) {
-    total += 1;
-  }
-
-  return total;
+export default function returnHowManyArguments(...args) {
+  eturn args.length;
 }
