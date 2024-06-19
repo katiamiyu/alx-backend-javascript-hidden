df894817c22392bfce6862bfff34d83982e79070
katiamiyu <katiamiyu@gmail.com>
2024-06-19 05:23:09 +0100

    task 0 -> 2

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Displays a message to STDOUT.
+ * @param {String} msg message to display.
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
